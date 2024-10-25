0f14bf010a4dd97aca15c89cfd844569d114b4f2
kennedy <kennedy.onyia@gmail.com>
2024-10-25 10:13:35 -0500

    removed debugging print statement

--- LSB.py
+++ LSB.py
@@ -5,6 +5,7 @@ from hist import PDH
 import numpy as np
 import os
 
+
 def read_LSB(img):
     count = 0
     output = 0
@@ -13,12 +14,12 @@ def read_LSB(img):
     for row in img:
         for col in row:
             for color in col:
-                if(bit >= 8):
-                    string+=chr(output)
+                if (bit >= 8):
+                    string += chr(output)
                     output = 0
                     bit = 0
                     count += 1
-                if(len(string) != 0 and ord(string[-1]) == 0):
+                if (len(string) != 0 and ord(string[-1]) == 0):
                     break
                 output = output | (color & 1) << bit
                 bit += 1
@@ -30,12 +31,12 @@ def write_LSB(img, data):
     bit = 0
     for i in range(len(img)):
         for j in range(len(img[i])):
-            for k in range(len(img[i,j])):
-                if(bit >= 8):
+            for k in range(len(img[i, j])):
+                if (bit >= 8):
                     index += 1
                     bit = 0
 
-                if(index < len(data)):
+                if (index < len(data)):
                     char = data[index]
                     if isinstance(char, str) and ord(char) <= 255:
                         value = ord(char)
@@ -45,19 +46,20 @@ def write_LSB(img, data):
                     img[i, j, k] = (img[i, j, k] & ~np.uint8(1)) | ((value & (np.uint8(1) << bit)) >> bit)
                     bit += 1
 
-                elif(index == len(data)):
-                    img[i,j,k] = img[i,j,k] & ~np.uint8(1)
+                elif (index == len(data)):
+                    img[i, j, k] = img[i, j, k] & ~np.uint8(1)
                     bit += 1
 
                 else:
                     break
+
 
 script_dir = os.path.dirname(__file__)
 rel_path = "text_files/50KB.txt"
 
 with open(os.path.join(script_dir, rel_path), 'r', encoding='utf-8', errors='ignore') as file:
     message = file.read()
-    print(message.encode('ascii', 'ignore').decode('ascii'))
+    message.encode('ascii', 'ignore').decode('ascii')
 cover = ImRead.from_file("yacht.bmp").pixel_array
 stego = np.copy(cover)
 write_LSB(stego, message)
