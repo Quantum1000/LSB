7faaa7e72933dfcbbb468e26b4bcdd952f8b66e9
Beeni Kharel <bxk200022@utdallas.edu>
2024-11-13 22:02:29 -0600

    Added a chart

--- psnr.py
+++ psnr.py
@@ -2,13 +2,12 @@ import numpy as np
 import pandas as pd
 import math
 
-df = pd.DataFrame(columns=["Image", "Text_Size", "LSB_PSNR", "LSXB_PSNR", "LSB_MSE", "LSXB_MSE", "Difference"])
+df = pd.DataFrame(columns=["Image", "Text_Size", "LSB_PSNR", "LSXB_PSNR", "Difference"])
 
 
 def calculate_mse(image1, image2):
     """Calculate the Mean Squared Error (MSE) between two images."""
     return np.mean((image1 - image2) ** 2)
-
 
 def calculate_psnr(original, stego):
     """Calculate the PSNR between two images."""
@@ -20,9 +19,8 @@ def calculate_psnr(original, stego):
     return psnr
 
 
-def add_psnr(image, txt_file, lsb_psnr, lsb_mse, lsxb_psnr, lsxb_mse):
+def add_psnr(df, image, txt_file, lsb_psnr, lsb_mse, lsxb_psnr, lsxb_mse):
     """Add the PSNR comparisons of LSB and LSXB implementation to df"""
-    global df
     psnr_difference = lsb_psnr - lsxb_psnr
 
     data = pd.DataFrame({
@@ -34,8 +32,5 @@ def add_psnr(image, txt_file, lsb_psnr, lsb_mse, lsxb_psnr, lsxb_mse):
         "LSXB_MSE": [lsxb_mse],
         "Difference": [psnr_difference]
     })
-    df = pd.concat([df, data], ignore_index=True)
+    return pd.concat([df, data], ignore_index=True)
 
-def view_psnr_comparison():
-    """Display the stored PSNR comparisons"""
-    print(df)
